87543748ab258a5adef20b325407130efc5a95ab
sumch76 <sumitchaubey76@gmail.com>
2024-12-05 23:08:21 +0530

    removed unnecesary comments

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,16 +3,16 @@ export const LOGO_URL="./images/icon.jpg";
 
 //card information url
 export const IMG_CDN_URL =
-  // "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+ 
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 
 // Image CDN URL for Restaurant Menu
 export const ITEM_IMG_CDN_URL =
-  // "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+  
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-// Swiggy API to get Restaurant data with foodfire erver
+// Swiggy API to get Restaurant data with foodfire server
 export const swiggy_api_URL =
   "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
